ce7a2e89cb9a13d5c9f8f12a092671957e729c57
ihollander <ianholla@gmail.com>
2021-11-01 16:32:54 -0400

    Refactored solution and added comments

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,27 +1,32 @@
 require_relative './stack'
 
-# your code here
 class MyQueue
   def initialize
     @s1 = Stack.new
     @s2 = Stack.new
   end
 
-  def add(element)
-    @s1.push(element)
-  end
-
-  def peek
-    @s2.push(@s1.pop) while @s1.peek
-    el = @s2.peek
-    @s1.push(@s2.pop) while @s2.peek
-    el
+  # We use two stacks so that we can re-order the items when adding a new item, 
+  # and move the added item to the bottom of the stack
+  def add(value)
+    # If we start with the following values before calling `#add(3)`
+    # @s1: [2, 1]    @s2: []
+    # First, take all the items out of @s1 and move them to @s2: 
+    # @s1: []        @s2: [1, 2]
+    @s2.push(@s1.pop) until @s1.empty? 
+    # Next, add a new item to the now-empty @s1: 
+    # @s1: [3]       @s2: [1, 2]
+    @s1.push(value)
+    # Finally, return all items to @s1 in the order they were removed: 
+    # @s1: [3, 2, 1] @s2: []
+    @s1.push(@s2.pop) until @s2.empty? 
   end
 
   def remove
-    @s2.push(@s1.pop) while @s1.peek
-    removed = @s2.pop
-    @s1.push(@s2.pop) while @s2.peek
-    removed
+    @s1.pop
+  end
+
+  def peek
+    @s1.peek
   end
 end
